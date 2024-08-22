5edc8be0b49962cb1c68a31b6b05c39af2e06288
Christina Peng <CPeng@tandemdiabetes.com>
2024-08-22 11:43:41 -0700

    sec5: Deleting an activity

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -4,9 +4,10 @@ import { Activity } from "../../../app/models/activity"
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({ activities, selectActivity } : Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity } : Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -21,6 +22,7 @@ export default function ActivityList({ activities, selectActivity } : Props) {
                             </Item.Description>
                             <Item.Extra>
                                 <Button onClick={() => selectActivity(act.id)} floated="right" content="View" color="blue"/>
+                                <Button onClick={() => deleteActivity(act.id)} floated="right" content="Delete" color="red"/>
                                 <Label basic content={act.category} />
                             </Item.Extra>
                         </Item.Content>
