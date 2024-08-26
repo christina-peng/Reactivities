57de045bc14593694d1c753acf20d038849ba534
Christina Peng <CPeng@tandemdiabetes.com>
2024-08-26 16:19:36 -0700

    sec8: Adding routes

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import App from "../layout/App";
+import HomePage from "../../features/activities/home/HopePage";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
-        element: <App />
+        element: <App />,
+        children: [
+            {path: '', element: <HomePage />},
+            {path: 'activities', element: <ActivityDashboard />},
+            {path: 'createActivity', element: <ActivityForm />}
+        ]
     }
 ]
 
