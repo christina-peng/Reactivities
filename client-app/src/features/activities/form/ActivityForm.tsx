f7aa6b4dba0e8996e3ebde438a8357f5691d624a
Christina Peng <CPeng@tandemdiabetes.com>
2024-08-27 11:27:55 -0700

    sec8: Redirect after submission

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,14 +2,16 @@ import { Button, Form, Segment } from "semantic-ui-react";
 import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LodingComponent";
+import {v4 as uuid} from "uuid";
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
     const {selectedActivity, createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
+    const navigate = useNavigate();
     const [activity, setActivity] = useState<Activity>({
         id: '',
         title: '',
@@ -25,7 +27,12 @@ export default observer(function ActivityForm() {
     }, [id, loadActivity])
 
     function handleSubmit() {
-        activity.id? updateActivity(activity) : createActivity(activity);
+        if (!activity.id) {
+            activity.id = uuid();
+            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        } else {
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }
     }
 
     function handleInputChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
